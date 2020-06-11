94ee3ce0f1b52ce4ff45fe6e9b065c8ab536b239
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-11 17:23:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,26 @@
 # Write your methods here
+def loop_message_five_times(message)
+  i = 0
+  while i < 5 do
+    puts message
+    i += 1
+end
 
+def loop_message_n_times(message, int)
+  int= 6
+  i = 0
+  while i < int do
+    puts message
+    i += 1
+end
 
+def output_array()
+  
+end
 
+def return_string_array()
+  
+end
 
 
 
